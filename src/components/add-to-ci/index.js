470f84d631f7b698dbf9d1c76afc23b2e2827d2d
Erik Hollensbe <github@hollensbe.org>
2019-06-17 05:10:09 -0700

    break out repo search from add to ci dialogs
    will be used for subscriptions too
    
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- src/components/add-to-ci/index.js
+++ src/components/add-to-ci/index.js
@@ -3,37 +3,28 @@ import React from 'react';
 import Client from '../../lib/client/client';
 
 import {handleError} from '../error-messages';
+import RepoSearch from '../repo-search';
 
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Snackbar from '@material-ui/core/Snackbar';
-import TextField from '@material-ui/core/TextField';
 import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 
 import blueGrey from '@material-ui/core/colors/blueGrey';
 
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import PeopleIcon from '@material-ui/icons/People';
-import SearchIcon from '@material-ui/icons/Search';
 
 class AddToCI extends React.Component {
   state = {
     scanning: false,
-    searching: false,
-    adding: false,
-    search: '',
-    matchedRepositories: [],
   };
 
   client = new Client();
-  searchTimer = null;
 
   startScan() {
     this.setState({scanning: true});
@@ -43,46 +34,20 @@ class AddToCI extends React.Component {
     });
   }
 
-  doSearch() {
-    this.client.repositoriesMyGet(
-      {search: this.state.search},
-      (err, res, resp) => {
-        this.setState({
-          matchedRepositories: res.slice(0, 10),
-          searching: false,
-        });
-      },
-    );
-  }
-
-  removeFromCI(repository) {
+  removeFromCI(repository, promise) {
     var [owner, repo] = repository.split('/', 2);
-    this.setState({adding: true});
     this.client.repositoriesCiDelOwnerRepoGet(owner, repo, (err, res, resp) => {
       handleError(err, resp);
-      this.setState({adding: false});
-      this.handleSearch(this.state.search);
+      promise();
     });
   }
 
-  addToCI(repository) {
+  addToCI(repository, promise) {
     var [owner, repo] = repository.split('/', 2);
-    this.setState({adding: true});
     this.client.repositoriesCiAddOwnerRepoGet(owner, repo, (err, res, resp) => {
       handleError(err, resp);
-      this.setState({adding: false});
-      this.handleSearch(this.state.search);
+      promise();
     });
-  }
-
-  handleSearch(chg) {
-    this.setState({searching: true, search: chg});
-    if (this.searchTimer) {
-      window.clearTimeout(this.searchTimer);
-      this.searchTimer = null;
-    }
-
-    this.searchTimer = window.setTimeout(this.doSearch.bind(this), 500);
   }
 
   render() {
@@ -119,44 +84,14 @@ class AddToCI extends React.Component {
               </Box>
             </Box>
           </ListItem>
-          <ListItem key="tinyci-searchitem">
-            <TextField
-              fullWidth
-              label="Search for a Repository"
-              placeholder="owner/repo"
-              onChange={chg => {
-                this.handleSearch(chg.target.value);
-              }}
-            />
-            {this.state.searching ? <MoreHorizIcon /> : <SearchIcon />}
-          </ListItem>
-          {this.state.matchedRepositories.map(elem => (
-            <ListItem key={elem.name}>
-              <Typography>{elem.name}</Typography>
-              {this.state.adding ? (
-                <MoreHorizIcon />
-              ) : elem.disabled ? (
-                <IconButton onClick={this.addToCI.bind(this, elem.name)}>
-                  <AddIcon />
-                </IconButton>
-              ) : (
-                <IconButton onClick={this.removeFromCI.bind(this, elem.name)}>
-                  <RemoveIcon />
-                </IconButton>
-              )}
-            </ListItem>
-          ))}
-          {this.state.matchedRepositories.length === 0 ? (
-            <ListItem key="tinyci-scanhelpitem">
-              <Typography>
-                Scan for items from remote resources by clicking the cloud icon
-                above, then search and add the repositories you wish to use in
-                tinyCI.
-              </Typography>
-            </ListItem>
-          ) : (
-            ''
-          )}
+          <RepoSearch
+            onAdd={(elem, promise) => {
+              this.addToCI(elem.name, promise);
+            }}
+            onRemove={(elem, promise) => {
+              this.removeFromCI(elem.name, promise);
+            }}
+          />
         </List>
         <Snackbar
           anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
---
+++ src/components/repo-search/index.js
@@ -0,0 +1,117 @@
+import React from 'react';
+
+import Client from '../../lib/client/client';
+
+import {handleError} from '../error-messages';
+
+import IconButton from '@material-ui/core/IconButton';
+import ListItem from '@material-ui/core/ListItem';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+import AddIcon from '@material-ui/icons/Add';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import RemoveIcon from '@material-ui/icons/Remove';
+import SearchIcon from '@material-ui/icons/Search';
+
+class RepoSearch extends React.Component {
+  state = {
+    changing: false,
+    search: '',
+    matchedRepositories: [],
+    searching: false,
+  };
+
+  client = new Client();
+  searchTimer = null;
+
+  handleSearch(chg, immediate) {
+    this.setState({searching: true, search: chg});
+    if (this.searchTimer) {
+      window.clearTimeout(this.searchTimer);
+      this.searchTimer = null;
+    }
+
+    if (!immediate) {
+      this.searchTimer = window.setTimeout(this.doSearch.bind(this), 500);
+    }
+  }
+
+  doSearch() {
+    this.client.repositoriesMyGet(
+      {search: this.state.search},
+      (err, res, resp) => {
+        if (!handleError(err, resp)) {
+          this.setState({
+            matchedRepositories: res.slice(0, 10),
+            searching: false,
+            changing: false,
+          });
+        }
+      },
+    );
+  }
+
+  performAction(action, elem) {
+    this.setState({changing: true});
+    action(elem, () => {
+      this.doSearch();
+      this.setState({changing: false});
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ListItem key="tinyci-searchitem">
+          <TextField
+            fullWidth
+            label="Search for a Repository"
+            placeholder="owner/repo"
+            onChange={chg => {
+              this.handleSearch(chg.target.value);
+            }}
+          />
+          {this.state.searching ? <MoreHorizIcon /> : <SearchIcon />}
+        </ListItem>
+        {this.state.matchedRepositories.map(elem => (
+          <ListItem key={elem.name}>
+            {this.state.changing ? (
+              <IconButton>
+                <MoreHorizIcon />
+              </IconButton>
+            ) : elem.disabled ? (
+              <IconButton
+                onClick={this.performAction.bind(this, this.props.onAdd, elem)}>
+                <AddIcon />
+              </IconButton>
+            ) : (
+              <IconButton
+                onClick={this.performAction.bind(
+                  this,
+                  this.props.onRemove,
+                  elem,
+                )}>
+                <RemoveIcon />
+              </IconButton>
+            )}
+            <Typography>{elem.name}</Typography>
+          </ListItem>
+        ))}
+        {this.state.matchedRepositories.length === 0 ? (
+          <ListItem key="tinyci-scanhelpitem">
+            <Typography>
+              Scan for items from remote resources by clicking the cloud icon
+              above, then search and add the repositories you wish to use in
+              tinyCI.
+            </Typography>
+          </ListItem>
+        ) : (
+          ''
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+export default RepoSearch;
