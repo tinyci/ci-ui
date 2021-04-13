fe39ba8ad2192a9edf848ac632b453ceb102a998
Erik Hollensbe <linux@hollensbe.org>
2021-04-12 17:57:26 -0700

    release tooling to integrate the k8s caddyfile
    Signed-off-by: Erik Hollensbe <linux@hollensbe.org>

--- box-dist.rb
+++ box-dist.rb
@@ -6,3 +6,9 @@ run "mkdir -p /ci-ui && tar -vxz --strip-components=1 -C /ci-ui -f /ci-ui.tar.gz
 copy "Caddyfile", "/Caddyfile"
 
 cmd %w[caddy run --config /Caddyfile]
+
+tag "tinyci/ui:latest"
+
+copy "Caddyfile.k8s", "/Caddyfile"
+
+tag "tinyci/ui:k8s"
