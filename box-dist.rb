38b2efa65ef53c7c22b480f15ccf21ce08db792f
Erik Hollensbe <linux@hollensbe.org>
2021-04-12 17:59:11 -0700

    Some build tweaks to adjust when images get tagged
    Signed-off-by: Erik Hollensbe <linux@hollensbe.org>

--- box-dist.rb
+++ box-dist.rb
@@ -3,12 +3,12 @@ from "caddy:2.3.0"
 skip { copy "ci-ui.tar.gz", "/" }
 
 run "mkdir -p /ci-ui && tar -vxz --strip-components=1 -C /ci-ui -f /ci-ui.tar.gz"
-copy "Caddyfile", "/Caddyfile"
-
 cmd %w[caddy run --config /Caddyfile]
 
-tag "tinyci/ui:latest"
+after do
+  copy "Caddyfile", "/Caddyfile"
+  tag "tinyci/ui:latest"
 
-copy "Caddyfile.k8s", "/Caddyfile"
-
-tag "tinyci/ui:k8s"
+  copy "Caddyfile.k8s", "/Caddyfile"
+  tag "tinyci/ui:k8s"
+end
