ac5e40be677e440abf16b1851b83d7fee66d1274
Erik Hollensbe <linux@hollensbe.org>
2021-04-16 18:35:00 -0700

    Fix names for "latest" tag
    Signed-off-by: Erik Hollensbe <linux@hollensbe.org>

--- box-dist.rb
+++ box-dist.rb
@@ -7,7 +7,7 @@ cmd %w[caddy run --config /Caddyfile]
 
 after do
   copy "Caddyfile", "/Caddyfile"
-  tag "tinyci/ui:#{getenv("VERSION") || "main" }-latest"
+  tag "tinyci/ui:#{getenv("VERSION") || "latest" }"
 
   copy "Caddyfile.k8s", "/Caddyfile"
   tag "tinyci/ui:#{getenv("VERSION") || "main"}-k8s"
